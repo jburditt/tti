2aec8f8d5c0b2b43e0c3a6d5c36377940b90af65
Arthur Newman <arthur.newman@belmar.ca>
2023-02-14 13:37:32 -0800

    Adding DEFAULT_AUTO_FIELD per suggestions from warnings.

--- fpo-api/fpo_api/settings.py
+++ fpo-api/fpo_api/settings.py
@@ -139,6 +139,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
